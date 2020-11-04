497d3af7692ee501c3e1ab447f6cd957e6f0c74c
unknown <daianagomes@id.uff.br>
2020-11-04 20:53:16 -0300

    consumo api

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FeedComponent } from './feed/feed.component';
+import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FeedComponent,
+    PerfilLateralComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
